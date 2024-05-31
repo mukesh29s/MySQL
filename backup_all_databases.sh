49728eaeb0668ffb0f32889caef74b38b19e38cd
mukesh29s <msingh.baghel@hotmail.com>
2024-05-31 16:32:43 +0200

    Update backup_all_databases.sh

--- backup_all_databases.sh
+++ backup_all_databases.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+# Script name: backup_all_databases.sh
+
 # MySQL credentials
 USER="your_mysql_username"
 PASSWORD="your_mysql_password"
 HOST="your_mysql_host"
 # Directory to save the dumps
-DUMP_DIR="/mysql_bck"
+DUMP_DIR="/path/to/your/dump_directory"
 
 # Ensure the dump directory exists
 mkdir -p $DUMP_DIR
 
 # Get the list of databases excluding the default ones
-DBS=$(mysql -u $USER -p$PASSWORD -h $HOST -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql|sys|tmp|x_dba|x_test1|s_test_s1)")
+DBS=$(mysql -u $USER -p$PASSWORD -h $HOST -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql|sys|tmp|x_dba)")
 
 # Loop through each database and dump it
 for DB in $DBS; do
